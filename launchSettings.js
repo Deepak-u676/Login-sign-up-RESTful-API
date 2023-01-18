06ca835aad7888f6833a48dac40dbbf48b55c8ab
dev <122309652+Deepak-u676@users.noreply.github.com>
2023-01-18 18:44:35 +0530

    initial commit

---
+++ launchSettings.js
@@ -0,0 +1,14 @@
+// "profiles":{
+//   "IIS Express": {
+//     "commandName": "IISExpress",
+//       "launchBrowser": true,
+//         "launchUrl": "swagger",
+//           "environmentVariables": {
+//       "ASPNETCORE_ENVIRONMENT": "Development"
+//     }
+//   },
+
+//   app.UseSwaggerUI(c => {
+//     c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiDemo v1");
+//     c.RoutePrefix = "swagger";
+//   });
