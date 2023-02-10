72ccaee88a3c02b920ee41886181b8613c69badc
deepak-u676 <devtesting676@gmail.com>
2023-02-10 20:45:46 +0530

    some chenges in frontend

--- frontend/dev/src/components/userReg.js
+++ frontend/dev/src/components/userReg.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../App.css";
 import UserLogin from './userLogin';
 
@@ -10,31 +10,29 @@ const UserReg = () => {
     const [userPassword, setUserPassword] = useState("")
     const [Conformpassword, setConformpassword] = useState("")
 
-    const regUser = (e) => {
-        e.preventDefault();
-        fetch("http://localhost:8000/api/register", {
+    const usserInfo = {
+        name: name,
+        firstName: firstName,
+        lastname: lastname,
+        userEmail: userEmail,
+        userPassword: userPassword,
+        Conformpassword: Conformpassword
+    }
+    useEffect = (() => {
+        fetch("api/register", {
             method: "POST",
-            body: JSON.stringify({
-                name: name,
-                firstName: firstName,
-                lastname: lastname,
-                userEmail: userEmail,
-                userPassword: userPassword,
-                Conformpassword: Conformpassword
-            }),
+            body: JSON.stringify({ usserInfo }),
             headers: { "contentType": "application/json" },
         })
-            .then((res) => res.json())
-            .then((data) =>
-                console.log(data)
-
+            .then(res => res.json())
+            .then(data => console.log(data)
             );
 
-    }
+    }, []);
     return (
         <>
             <h3> User Register</h3>
-            <form className='mainContainer' onSubmit={regUser}>
+            <form className='mainContainer' onSubmit={usserInfo}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label" >Name</label>
                     <input type="text" className="form-control" aria-describedby="emailHelp" onChange={(e) => setname(e.target.value)} />
